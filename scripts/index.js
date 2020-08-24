2e2f83aa1d0e3182d3fe5efb50622d542a419e61
Kevin Jeffers <jevinkeffers@gmail.com>
2020-08-24 17:03:18 -0400

    Added current weather for Atlanta

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-'use strict';
+fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=2ba30299663ddd2ea7871a5e569b1def&units=imperial"
+    )
+    .then(response => response.json())
+    .then(body => {
+        document.getElementById("weather").innerHTML = body.main.temp + "°";
+    })
+    .catch(error => alert("Please add your api key to the fetch url"));
